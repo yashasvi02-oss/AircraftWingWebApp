0222d62b839b474119299224ade75ae0647aac65
Yashasvi <yashasviclg@gmail.com>
2026-01-12 17:14:44 +0530

    Add requirements file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+    from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -47,5 +47,8 @@ def calculate():
         status=status
     )
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
